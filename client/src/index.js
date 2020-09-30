5d3c1fcf0ac161d83f49994f0ca6d0c5a2289e35
elRava <giuseppe.ravagnani@gmail.com>
2020-09-30 23:29:43 +0200

    changed client template

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Modal from "react-modal";
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-//import './App.css';
-import './assets/scss/style.scss';
-
-const history = createBrowserHistory();
+Modal.setAppElement("#root");
 
 ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
-  document.getElementById('root')
+  <App />,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
