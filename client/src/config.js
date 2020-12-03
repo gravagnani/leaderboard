38a30b2414b82f98c803b631c1794977b9940937
elRava <giuseppe.ravagnani@gmail.com>
2020-12-04 00:50:54 +0100

    debug deploy on pi

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,7 @@
-const BASE_URL = "http://localhost:5000";
+//const BASE_URL = "http://localhost:5000";
+const BASE_URL =
+	process.env.NODE_ENV === "development"
+		? "http://localhost:5000"
+		: window.location.origin;
 
 export { BASE_URL };
